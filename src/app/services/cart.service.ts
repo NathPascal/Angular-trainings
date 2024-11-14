17bf8797e0230afc829547715b55fe4cf21c7a10
NathPascal <nath.pascal03@gmail.com>
2024-11-14 15:33:40 +0100

    add local storage

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -12,12 +12,15 @@ export class CartService {
   cart: Training[] = [];
   customer = { name: '', firstName: '', address: '', phone: '', email: '' };
 
-  constructor() { }
+  constructor() { 
+    this.storageCart(); // initialisation 
+
+  }
 
   // Methode pour ajouter les éléments dans le panier
   addTraining(training: Training): void {
     this.cart.push(training);
-  
+    this.saveCart(); // sauvegarde dans local storage
   }
 
   // Methode tous les éléments dans le panier
@@ -28,6 +31,20 @@ export class CartService {
   // Methode pour supprimer un élément
   removeTraining(training: Training): void {
     this.cart = this.cart.filter(item => item.id !== training.id);
+    this.saveCart(); //update sauvegarde local storage
+  }
+
+  // sauvegarde panier dans local storage
+  private saveCart(): void {
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+
+  // initialise panier dans local storage
+  private storageCart(): void {
+    const cartData = localStorage.getItem('cart');
+    if (cartData) {
+      this.cart = JSON.parse(cartData);
+    }
   }
 
   getCustomer() {
